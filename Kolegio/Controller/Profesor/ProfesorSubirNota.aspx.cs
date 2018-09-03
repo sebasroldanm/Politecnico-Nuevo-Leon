0b96665606948a99bee0744253b51dc46cef107c
Cristhian Camilo Rodriguez Galindo <camilorodri28@outlook.com>
2018-09-02 19:21:37 -0500

    Revert "Rol Profesor - Formulario Subir Nota"
    This reverts commit 74c01e1aa4ec9db7dae7d420f0ff90e583f8cc15.

--- Kolegio/Controller/Profesor/ProfesorSubirNota.aspx.cs
+++ Kolegio/Controller/Profesor/ProfesorSubirNota.aspx.cs
@@ -5,9 +5,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using Logica;
-using Utilitarios;
-using Datos;
 
 public partial class View_Profesor_ProfesorSubirNota : System.Web.UI.Page
 {
@@ -22,11 +19,11 @@ public partial class View_Profesor_ProfesorSubirNota : System.Web.UI.Page
             año = año + "-01-01";
             DataTable re = datos.obtenerAniodeCurso(año);
             Session["anio"] = re.Rows[0]["id_anio"];
-            btn_Subirnota.Visible = false;
+            btn_Subirnota.Visible = true;
             ButtonVerNota.Visible = true;
         }
         else
-            Response.Redirect("~/View/Acudiente/AccesoDenegado.aspx");
+            Response.Redirect("/View/Acudiente/AccesoDenegado.aspx");
         
     }
 
@@ -37,35 +34,65 @@ public partial class View_Profesor_ProfesorSubirNota : System.Web.UI.Page
 
     protected void btn_Subirnota_Click(object sender, EventArgs e)
     {
-        DUser datos = new DUser();
-        UUser enc = new UUser();
-        LReg logic = new LReg();
+        if (ddl_alumno.SelectedValue == "0" || ddl_materia.SelectedValue == "0" || ddt_curso.SelectedValue == "0")
+        {
+            L_Error.Text = "Falta seleccionar";
+        }
+        else
+        {
+            DaoUser datos = new DaoUser();
+            EUser enc = new EUser();
 
-        enc = logic.subirNota(ddl_alumno.SelectedValue, ddl_materia.SelectedValue, ddt_curso.SelectedValue, tb_nt.Text, tb_nt2.Text, tb_nt3.Text);
-        L_Error.Text = enc.Mensaje;
+            enc.Id_estudiante = ddl_alumno.SelectedValue;
+            enc.Materia = ddl_materia.SelectedValue;
+            enc.Curso = ddt_curso.SelectedValue;
+            DataTable registros = datos.obtenerNota(enc);
 
-        tb_denifitiva.Text = enc.Notadef;
-        ButtonVerNota.Visible = true;
-        btn_Subirnota.Visible = false;
- 
+            enc.IdNota = registros.Rows[0]["id_nota"].ToString();
+            Double n1 = Convert.ToDouble(tb_nt.Text);
+            Double n2 = Convert.ToDouble(tb_nt2.Text);
+            Double n3 = Convert.ToDouble(tb_nt3.Text);
+
+            Double nd = (n1 + n2 + n3) / 3.0;
+
+            enc.Nota1 = n1.ToString();
+            enc.Nota2 = n2.ToString();
+            enc.Nota3 = n3.ToString();
+        
+            enc.Notadef = nd.ToString();
+            tb_denifitiva.Text = nd.ToString();
+
+            datos.insertarNota(enc);
+            ButtonVerNota.Visible = false;
+            btn_Subirnota.Visible = true;
+        }
+            
     }
 
     protected void ButtonVerNota_Click(object sender, EventArgs e)
     {
-            DUser datos = new DUser();
-            UUser enc = new UUser();
-            LReg logic = new LReg();
+        if (ddl_alumno.SelectedValue == "0" || ddl_materia.SelectedValue == "0" || ddt_curso.SelectedValue == "0")
+        {
+            L_Error.Text = "Falta seleccionar";
+        }
+        else
+        {
+            DaoUser datos = new DaoUser();
+            EUser enc = new EUser();
 
-            enc = logic.verNota(ddl_alumno.SelectedValue, ddl_materia.SelectedValue, ddt_curso.SelectedValue);
+            enc.Id_estudiante = ddl_alumno.SelectedValue;
+            enc.Materia = ddl_materia.SelectedValue;
+            enc.Curso = ddt_curso.SelectedValue;
+            DataTable registros =  datos.obtenerNota(enc);
 
-            tb_nt.Text = enc.Nota1;
-            tb_nt2.Text = enc.Nota2; 
-            tb_nt3.Text = enc.Nota3;
-            tb_denifitiva.Text = enc.Notadef;
-
-        L_Error.Text = enc.Mensaje;
-
-        btn_Subirnota.Visible = true;
+            tb_nt.Text = registros.Rows[0]["nota1"].ToString();
+            tb_nt2.Text =  registros.Rows[0]["nota2"].ToString(); 
+            tb_nt3.Text = registros.Rows[0]["nota3"].ToString();
+      
+            tb_denifitiva.Text = registros.Rows[0]["notadef"].ToString();
+        }
+        
+            
 
     }
 }
