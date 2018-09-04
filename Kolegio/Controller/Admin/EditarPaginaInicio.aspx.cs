4505c6ce29b59847aebbe5112070dc0847d8e80e
CamiloRodri <camilorodri28@outlook.com>
2018-09-03 19:27:01 -0500

    Configuracion Pagina Edward Ramos

--- Kolegio/Controller/Admin/EditarPaginaInicio.aspx.cs
+++ Kolegio/Controller/Admin/EditarPaginaInicio.aspx.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Utilitarios;
+using Datos;
+using Logica;
 
 public partial class View_Admin_EditarPaginaInicio : System.Web.UI.Page
 {
@@ -24,46 +27,80 @@ public partial class View_Admin_EditarPaginaInicio : System.Web.UI.Page
 
     protected void B_Modificar_Click(object sender, EventArgs e)
     {
-        EUser Edusua = new EUser();
-        DaoUser datos = new DaoUser();
 
-        Edusua.Inicio = TB_Nosotros.Text;
-        Edusua.Mision = TB_Mision.Text;
-        Edusua.Vision = TB_Vision.Text;
-        Edusua.Session = Session.SessionID;
 
-        DataTable registros = datos.editarInicio(Edusua);
-        this.RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Datos Modificados.');window.location=\"EditarPaginaInicio.aspx\"</script>");
+        LUser logica = new LUser();
+        UUser usua = new UUser();
+
+        usua = logica.ModificarPaginaInicio(
+            TB_Nosotros.Text,
+            TB_Mision.Text,
+            TB_Vision.Text,
+            Session.SessionID
+            );
+
+        this.Page.Response.Write(usua.Notificacion);
+
+
+        //EUser Edusua = new EUser();
+        //DaoUser datos = new DaoUser();
+
+        //Edusua.Inicio = TB_Nosotros.Text;
+        //Edusua.Mision = TB_Mision.Text;
+        //Edusua.Vision = TB_Vision.Text;
+        //Edusua.Session = Session.SessionID;
+
+        //DataTable registros = datos.editarInicio(Edusua);
+        //this.RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Datos Modificados.');window.location=\"EditarPaginaInicio.aspx\"</script>");
     }
 
 
     protected void B_Traer_Click(object sender, EventArgs e)
     {
-        EUser usua = new EUser();
-        DaoUser dat = new DaoUser();
 
-        DataTable registros = dat.incio();
+        LUser logica = new LUser();
+        UUser usua = new UUser();
 
-        if (registros.Rows.Count > 0)
-        {
-            TB_Nosotros.Text = Convert.ToString(registros.Rows[0]["inicio_cont"].ToString());
-            TB_Mision.Text = Convert.ToString(registros.Rows[0]["mision_inicio"].ToString());
-            TB_Vision.Text = Convert.ToString(registros.Rows[0]["vision_inicio"].ToString());
-            
-        }
+        usua = logica.TraerDatosPagina();
+
+        TB_Nosotros.Text = usua.Nosotros;
+        TB_Vision.Text = usua.Vision;
+        TB_Mision.Text = usua.Mision;
+
+
+        //EUser usua = new EUser();
+        //DaoUser dat = new DaoUser();
+
+        //DataTable registros = dat.incio();
+
+        //if (registros.Rows.Count > 0)
+        //{
+        //    TB_Nosotros.Text = Convert.ToString(registros.Rows[0]["inicio_cont"].ToString());
+        //    TB_Mision.Text = Convert.ToString(registros.Rows[0]["mision_inicio"].ToString());
+        //    TB_Vision.Text = Convert.ToString(registros.Rows[0]["vision_inicio"].ToString());
+
+        //}
     }
 
     protected void B_Terminaranio_Click(object sender, EventArgs e)
     {
-        DaoUser datos = new DaoUser();
-        EUser enc = new EUser();
 
-        enc.Año = fechanac.Text;
-        datos.editaFinaño(enc);
+        LUser logica = new LUser();
+        UUser usua = new UUser();
 
-        bool ok = true;
-        datos.editaBool(ok);
+        usua = logica.insertarfechafin(fechanac.Text);
 
-        this.Page.Response.Write("<script language='JavaScript'>window.alert('Insertado con Exito');</script>");
+        this.Page.Response.Write(usua.Notificacion);
+
+        //DaoUser datos = new DaoUser();
+        //EUser enc = new EUser();
+
+        //enc.Año = fechanac.Text;
+        //datos.editaFinaño(enc);
+
+        //bool ok = true;
+        //datos.editaBool(ok);
+
+        //this.Page.Response.Write("<script language='JavaScript'>window.alert('Insertado con Exito');</script>");
     }
 }
