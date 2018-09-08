438304446b8a9f801dc88596497a86f12317079f
CamiloRodri <camilorodri28@outlook.com>
2018-09-08 09:23:08 -0500

    Referencias MAC

--- Utilitarios/MAC.cs
+++ Utilitarios/MAC.cs
@@ -1,13 +1,4 @@
 ﻿using System;
-using System.Data;
-using System.Configuration;
-using System.Web;
-using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
-using System.Runtime.InteropServices;
 using System.Net;
 using System.Net.Sockets;
 using System.Net.NetworkInformation;
