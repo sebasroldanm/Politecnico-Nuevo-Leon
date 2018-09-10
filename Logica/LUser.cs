96781eb77278c477dcec979d8e633365b278d56a
CamiloRodri <camilorodri28@outlook.com>
2018-09-10 09:43:03 -0500

    Eliminar Referencias Inutiles.

--- Logica/LUser.cs
+++ Logica/LUser.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using Datos;
 using Utilitarios;
 using System.Data;
