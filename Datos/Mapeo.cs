9a8e24c5cf9a39a977006b4601bd1dc4b18a9669
CamiloRodri <camilorodri28@outlook.com>
2018-09-28 16:15:22 -0500

    Mapeo - Creacion de Clase

---
+++ Datos/Mapeo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Datos
+{
+    public class Mapeo : DbContext
+    {
+        static Mapeo()
+        {
+            Database.SetInitializer<Mapeo>(null);
+        }
+        private readonly string schema;
+
+        public Mapeo(string schema)
+            : base("name=casel")
+        {
+            this.schema = schema;
+        }
+
+
+
+
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            builder.HasDefaultSchema(this.schema);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
