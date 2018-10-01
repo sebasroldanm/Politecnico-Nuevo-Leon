ddf7aa1f51ac4e676cfc67d623f1dfa14e35bdc8
CamiloRodri <camilorodri28@outlook.com>
2018-10-01 15:46:36 -0500

    Acudiente Tablas Mapeo

---
+++ Utilitarios/Acudiente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Utilitarios
+{
+    [Serializable]
+    [Table("acudiente", Schema = "usuario")]
+    public class Acudiente
+    {
+        private Int32 idAc;
+        private Int32 idEstudiante;
+        private Int32 idAcudiente;
+
+        [Key]
+        [Column("id_ac")]
+        public int IdAc { get => idAc; set => idAc = value; }
+        [Column("id_ac_estudiante")]
+        public int IdEstudiante { get => idEstudiante; set => idEstudiante = value; }
+        [Column("id_ac_acudiente")]
+        public int IdAcudiente { get => idAcudiente; set => idAcudiente = value; }
+    }
+}
---
+++ Utilitarios/RolUsuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Utilitarios
+{
+    [Serializable]
+    [Table("rol_usuario", Schema = "usuario")]
+    public class RolUsuario
+    {
+        private Int32 idRol;
+        private String rolUsua;
+
+        [Key]
+        [Column("id_rol")]
+        public int IdRol { get => idRol; set => idRol = value; }
+        [Column("nombre_rol")]
+        public string RolUsua { get => rolUsua; set => rolUsua = value; }
+    }
+}
