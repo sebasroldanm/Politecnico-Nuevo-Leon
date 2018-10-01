ec5a51bc540a92fb91084061390a0dbcbaebf445
CamiloRodri <camilorodri28@outlook.com>
2018-10-01 15:53:56 -0500

    LMUser

---
+++ Logica/LMUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using Utilitarios;
+
+namespace Logica
+{
+    public class LMUser
+    {
+        public void insertarUserMapeo(Usuario text)
+        {
+            DUser dao = new DUser();
+            dao.insertarUserMapeo(text);
+        }
+
+    }
+}
