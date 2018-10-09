ba92d9bab6fdf6fe72bf3039f0bad6fd9422d64a
edwardconw <coyoteluxeredward@outlook.es>
2018-10-09 18:03:14 -0500

    Tablas Dep - Ciudad

---
+++ Utilitarios/Mlugares/Ciudad.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Utilitarios.Mlugares
+{
+
+    [Serializable]
+    [Table("ciudad",Schema = "lugares")]
+    public class Ciudad
+    {
+        Int32 idCiudad;
+        String nombreCiudad;
+        Int32 idCDepart;
+
+        [Key]
+        [Column("id_ciudad")]
+        public int id_ciudad { get => idCiudad; set => idCiudad = value; }
+        [Column("nombre_ciudad")]
+        public string nombre_ciudad { get => nombreCiudad; set => nombreCiudad = value; }
+        [Column("id_c_depart")]
+        public int id_c_depart { get => idCDepart; set => idCDepart = value; }
+
+    }
+}
---
+++ Utilitarios/Mlugares/Departamento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Utilitarios.Mlugares
+{
+
+
+    [Serializable]
+    [Table("departamento", Schema ="lugares")]
+    public class Departamento
+    {
+        Int32 idDep;
+        String nomDep;
+
+        [Key]
+        [Column("id_dep")]
+        public int id_dep { get => idDep; set => idDep = value; }
+        [Column("nom_dep")]
+        public string nom_dep { get => nomDep; set => nomDep = value; }
+        
+    }
+}
