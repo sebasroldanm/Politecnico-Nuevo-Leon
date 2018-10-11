7a674e4887acfd071f68668d5bc9370249ffb272
edwardconw <coyoteluxeredward@outlook.es>
2018-10-11 14:53:50 -0500

    Vista Acudiente Estudiante

---
+++ Utilitarios/AcudienteEstudianteVista.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilitarios
+{
+    public class  AcudienteEstudianteVista
+    {
+
+
+        private Int32 idUsua;
+        private String nombreUsua;
+
+        public int IdUsua { get => idUsua; set => idUsua = value; }
+        public string NombreUsua { get => nombreUsua; set => nombreUsua = value; }
+
+    }
+}
