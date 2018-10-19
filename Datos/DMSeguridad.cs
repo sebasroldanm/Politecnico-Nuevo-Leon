374343a5ec9df3e431d653dbb246e83f38d4b395
CamiloRodri <camilorodri28@outlook.com>
2018-10-19 16:47:24 -0500

    Seguridad Inicio

--- Datos/DMSeguridad.cs
+++ Datos/DMSeguridad.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using Utilitarios;
+using Utilitarios.MSeguridad;
+using Utilitarios.MEncSeguridad;
 
 namespace Datos
 {
     public class DMSeguridad
     {
+        public void fiel_auditoria_inicio(string _accion, string sesion, MEncInicio enc)
+        {
+            Auditoria au = new Auditoria();
+            USeguridad s = new USeguridad();
+            au.fecha = DateTime.Now.ToShortDateString() +" "+ DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second;
+            au.accion= _accion;
+            au.schema = "inicio";
+            au.tabla = "inicio";
+            au.pk = "1";
+            au.session = sesion;
+            au.user_bd = "postgres";
+            if(_accion == "UPDATE")
+            {
+                au.data = JsonConvert.SerializeObject(enc);
+                using (var db = new Mapeo("public"))
+                {
+                    db.auditoria.Add(au);
+                    db.SaveChanges();
+                }
+
+            }
+        }
     }
 }
---
+++ Utilitarios/USeguridad.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilitarios
+{
+    public class USeguridad
+    {
+        private String _data_new;
+        private String _data_old;
+
+        private String fecha;
+        private String _accion;
+        private String _session;
+        private String _user_bd;
+        private String _table_pk;
+        private String schema;
+        private String tabla;
+        private String _init;
+        private String Data;
+
+        public string Data_new { get => _data_new; set => _data_new = value; }
+        public string Data_old { get => _data_old; set => _data_old = value; }
+
+
+        public string Accion { get => _accion; set => _accion = value; }
+        public string Session { get => _session; set => _session = value; }
+        public string User_bd { get => _user_bd; set => _user_bd = value; }
+        public string Table_pk { get => _table_pk; set => _table_pk = value; }
+        public string Init { get => _init; set => _init = value; }
+        public string Schema { get => schema; set => schema = value; }
+        public string Fecha { get => fecha; set => fecha = value; }
+        public string Tabla { get => tabla; set => tabla = value; }
+        public string data { get => Data; set => Data = value; }
+    }
+}
