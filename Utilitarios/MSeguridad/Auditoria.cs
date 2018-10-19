cef95dc0e48dc7fa9ca4d55d50b6085c8ba426df
CamiloRodri <camilorodri28@outlook.com>
2018-10-19 00:21:16 -0500

    Tabla Seguridad

---
+++ Utilitarios/MSeguridad/Auditoria.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Utilitarios.MSeguridad
+{
+    [Serializable]
+    [Table("auditoria", Schema = "security")]
+    public class Auditoria
+    {
+        private Int32 Id;
+        private String Fecha;
+        private String Accion;
+        private String Esquema;
+        private String Tabla;
+        private String Session;
+        private String UserBD;
+        private String Data;
+        private String Pk;
+
+        [Key]
+        [Column("id")]
+        public int id { get => Id; set => Id = value; }
+        [Column("fecha")]
+        public string fecha { get => Fecha; set => Fecha = value; }
+        [Column("accion")]
+        public string accion { get => Accion; set => Accion = value; }
+        [Column("schema")]
+        public string schema { get => Esquema; set => Esquema = value; }
+        [Column("tabla")]
+        public string tabla { get => Tabla; set => Tabla = value; }
+        [Column("session")]
+        public string session { get => Session; set => Session = value; }
+        [Column("user_bd")]
+        public string user_bd { get => UserBD; set => UserBD = value; }
+        [Column("data")]
+        public string data { get => Data; set => Data = value; }
+        [Column("pk")]
+        public string pk { get => Pk; set => Pk = value; }
+    }
+}
---
+++ Utilitarios/MSeguridad/Autenticacion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Utilitarios.MSeguridad
+{
+    [Serializable]
+    [Table("autenication", Schema = "security")]
+    public class Autenticacion
+    {
+        private Int32 Id;
+        private Int32 UserId;
+        private String Ip;
+        private String Mac;
+        private String FechaInicio;
+        private String Sesion;
+        private String FechaFin;
+
+        [Key]
+        [Column("id")]
+        public int id { get => Id; set => Id = value; }
+        [Column("user_id")]
+        public int user_id { get => UserId; set => UserId = value; }
+        [Column("ip")]
+        public string ip { get => Ip; set => Ip = value; }
+        [Column("mac")]
+        public string mac { get => Mac; set => Mac = value; }
+        [Column("fecha_inicio")]
+        public string fecha_inicio { get => FechaInicio; set => FechaInicio = value; }
+        [Column("session")]
+        public string session { get => Sesion; set => Sesion = value; }
+        [Column("fecha_fin")]
+        public string fecha_fin { get => FechaFin; set => FechaFin = value; }
+
+        
+
+    }
+}
