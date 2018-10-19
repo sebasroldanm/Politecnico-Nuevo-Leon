0fc76e8f029783b228abada54177734cd1815c6f
edwardconw <coyoteluxeredward@outlook.es>
2018-10-19 18:06:23 -0500

    Descartar idioma profesor

--- Kolegio/Controller/Admin/AgregarProfesor.aspx.cs
+++ Kolegio/Controller/Admin/AgregarProfesor.aspx.cs
@@ -106,7 +106,7 @@ public partial class View_Admin_AgregarProfesor : System.Web.UI.Page
 
     protected void descartar_idioma_Click(object sender, EventArgs e)
     {
-        LIdioma logica = new LIdioma();
+        LMIdioma logica = new LMIdioma();
         UIdioma enc = new UIdioma();
 
         int idioma = Convert.ToInt32(Session["nombreIdioma"]);
