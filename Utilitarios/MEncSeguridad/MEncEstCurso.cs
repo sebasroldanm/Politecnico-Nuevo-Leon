173619b171288ee10d3c42839790ad62bb360ee4
edwardconw <coyoteluxeredward@outlook.es>
2018-10-22 16:31:42 -0500

    Enc Seguridad Materia Fecha

--- Utilitarios/MEncSeguridad/MEncEstCurso.cs
+++ Utilitarios/MEncSeguridad/MEncEstCurso.cs
@@ -17,13 +17,13 @@ namespace Utilitarios.MEncSeguridad
         private String Id_ec_estudiante_anterior;
         private String Id_ec_curso_anterior;
 
-        public string id_ec_nuevo1 { get => Id_ec_nuevo; set => Id_ec_nuevo = value; }
-        public string id_ec_estudiante_nuevo1 { get => Id_ec_estudiante_nuevo; set => Id_ec_estudiante_nuevo = value; }
-        public string id_ec_curso_nuevo1 { get => Id_ec_curso_nuevo; set => Id_ec_curso_nuevo = value; }
-        public string id_ec_anterior1 { get => Id_ec_anterior; set => Id_ec_anterior = value; }
-        public string id_ec_estudiante_old1 { get => Id_ec_estudiante_old; set => Id_ec_estudiante_old = value; }
-        public string id_ec_curso_old1 { get => Id_ec_curso_old; set => Id_ec_curso_old = value; }
-        public string id_ec_estudiante_anterior1 { get => Id_ec_estudiante_anterior; set => Id_ec_estudiante_anterior = value; }
-        public string id_ec_curso_anterior1 { get => Id_ec_curso_anterior; set => Id_ec_curso_anterior = value; }
+        public string id_ec_nuevo { get => Id_ec_nuevo; set => Id_ec_nuevo = value; }
+        public string id_ec_estudiante_nuevo { get => Id_ec_estudiante_nuevo; set => Id_ec_estudiante_nuevo = value; }
+        public string id_ec_curso_nuevo { get => Id_ec_curso_nuevo; set => Id_ec_curso_nuevo = value; }
+        public string id_ec_anterior { get => Id_ec_anterior; set => Id_ec_anterior = value; }
+        public string id_ec_estudiante_old { get => Id_ec_estudiante_old; set => Id_ec_estudiante_old = value; }
+        public string id_ec_curso_old { get => Id_ec_curso_old; set => Id_ec_curso_old = value; }
+        public string id_ec_estudiante_anterior { get => Id_ec_estudiante_anterior; set => Id_ec_estudiante_anterior = value; }
+        public string id_ec_curso_anterior { get => Id_ec_curso_anterior; set => Id_ec_curso_anterior = value; }
     }
 }
