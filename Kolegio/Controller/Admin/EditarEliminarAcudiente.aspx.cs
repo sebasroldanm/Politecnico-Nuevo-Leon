10fcb1c4e530cd04820a50e9079c1de12a7042da
CamiloRodri <camilorodri28@outlook.com>
2018-10-23 11:32:34 -0500

    Agregar Sql Server

--- Kolegio/Controller/Admin/EditarEliminarAcudiente.aspx.cs
+++ Kolegio/Controller/Admin/EditarEliminarAcudiente.aspx.cs
@@ -274,7 +274,7 @@ public partial class View_Admin_EditarEliminarAcudiente : System.Web.UI.Page
     }
     protected string cargarImagen()
     {
-        LUser logic = new LUser();
+        LMUser logic = new LMUser();
         UUser enc = new UUser();
         enc = logic.CargaFotoM(System.IO.Path.GetFileName(FileUpload1.PostedFile.FileName), System.IO.Path.GetExtension(FileUpload1.PostedFile.FileName), FileUpload1.ToString(), Server.MapPath("~/FotosUser"), int.Parse(Session["idioma"].ToString()));
         try
--- Kolegio/Controller/Admin/EditarEliminarProfesor.aspx.cs
+++ Kolegio/Controller/Admin/EditarEliminarProfesor.aspx.cs
@@ -269,7 +269,7 @@ public partial class View_Admin_EditarEliminarProfesor : System.Web.UI.Page
 
     protected string cargarImagen()
     {
-        LUser logic = new LUser();
+        LMUser logic = new LMUser();
         UUser enc = new UUser();
         enc = logic.CargaFotoM(System.IO.Path.GetFileName(FileUpload1.PostedFile.FileName), System.IO.Path.GetExtension(FileUpload1.PostedFile.FileName), FileUpload1.ToString(), Server.MapPath("~/FotosUser"), int.Parse(Session["idioma"].ToString()));
         try
