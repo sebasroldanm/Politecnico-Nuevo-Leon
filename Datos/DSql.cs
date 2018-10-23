78d27ee05f0993885f2ab87942de3d31d35dfca0
CamiloRodri <camilorodri28@outlook.com>
2018-10-23 18:51:58 -0500

    Datos SQL Server

---
+++ Datos/DSql.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Datos
+{
+    public class DSql
+    {
+        private SqlConnection conection = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlServer"].ConnectionString);
+        private DataSet ds;
+
+        public DataTable listaacuestu()
+        {
+            try
+            {
+                conection.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM usuario.listaestuacu()", conection);
+                SqlDataAdapter ad = new SqlDataAdapter(cmd);
+
+                ds = new DataSet();
+                ad.Fill(ds, "tabla");
+
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+            finally
+            {
+                if (conection != null)
+                {
+                    conection.Close();
+                }
+            }
+
+            return ds.Tables["tabla"];
+        }
+    }
+}
--- Utilitarios/ListaAcudiente.cs
+++ Utilitarios/ListaAcudiente.cs
@@ -3,36 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Utilitarios
 {
+    [Serializable]
+    [Table("usuario", Schema = "estudianteacudiente")]
     public class ListaAcudiente
     {
-        private String Fotoacudiente;
-        private String Apeacudiente;
-        private String Nomacudiente;
-        private String Docacudiente;
-        private String Telefonoacudiente;
-        private String Useracudiente;
-        private String Claveacudiente;
-        private String Fotoestudiente;
+        private Int32 IdEstudiante;
         private String Nomestudiante;
         private String Apeestudiante;
-        private String Docestudiante;
+        private String Userestudiante;
+        private String Claveestudiante;
         private String Telefonoestudiante;
+        private String Fotoestudiante;
+        private Int32 Docestudiante;
+        private Int32 Id_es;
+        private Int32 Idacudiente;
+        private String Nomacudiente;
+        private String Apeacudiente;
+        private String Useracudiente;
+        private String Claveacudiente;
+        private String Telefonoacudiente;
+        private String Fotoacudiente;
+        private Int32 Docacudiente;
+        private Int32 Id_ac;
+        
+        
+        
+        
 
-
-        public string fotoacudiente { get => Fotoacudiente; set => Fotoacudiente = value; }
-        public string apeacudiente { get => Apeacudiente; set => Apeacudiente = value; }
-        public string nomacudiente { get => Nomacudiente; set => Nomacudiente = value; }
-        public string docacudiente { get => Docacudiente; set => Docacudiente = value; }
-        public string telefonoacudiente { get => Telefonoacudiente; set => Telefonoacudiente = value; }
-        public string useracudiente { get => Useracudiente; set => Useracudiente = value; }
-        public string claveacudiente { get => Claveacudiente; set => Claveacudiente = value; }
-        public string fotoestudiente { get => Fotoestudiente; set => Fotoestudiente = value; }
+        [Key]
+        [Column("idestudiante")]
+        public int idestudiante { get => IdEstudiante; set => IdEstudiante = value; }
+        [Column("nomestudiante")]
         public string nomestudiante { get => Nomestudiante; set => Nomestudiante = value; }
+        [Column("apeestudiante")]
         public string apeestudiante { get => Apeestudiante; set => Apeestudiante = value; }
-        public string docestudiante { get => Docestudiante; set => Docestudiante = value; }
+        [Column("userestudiante")]
+        public string userestudiante { get => Userestudiante; set => Userestudiante = value; }
+        [Column("claveestudiante")]
+        public string claveestudiante { get => Claveestudiante; set => Claveestudiante = value; }
+        [Column("telefonoestudiante")]
         public string telefonoestudiante { get => Telefonoestudiante; set => Telefonoestudiante = value; }
+        [Column("fotoestudiante")]
+        public string fotoestudiante { get => Fotoestudiante; set => Fotoestudiante = value; }
+        [Column("docestudiante")]
+        public int docestudiante { get => Docestudiante; set => Docestudiante = value; }
+        [Column("id_es")]
+        public int id_es { get => Id_es; set => Id_es = value; }
+        [Column("idacudiente")]
+        public int idacudiente { get => Idacudiente; set => Idacudiente = value; }
+        [Column("nomacudiente")]
+        public string nomacudiente { get => Nomacudiente; set => Nomacudiente = value; }
+        [Column("apeacudiente")]
+        public string apeacudiente { get => Apeacudiente; set => Apeacudiente = value; }
+        [Column("useracudiente")]
+        public string useracudiente { get => Useracudiente; set => Useracudiente = value; }
+        [Column("claveacudiente")]
+        public string claveacudiente { get => Claveacudiente; set => Claveacudiente = value; }
+        [Column("telefonoacudiente")]
+        public string telefonoacudiente { get => Telefonoacudiente; set => Telefonoacudiente = value; }
+        [Column("fotoacudiente")]
+        public string fotoacudiente { get => Fotoacudiente; set => Fotoacudiente = value; }
+        [Column("docacudiente")]
+        public int docacudiente { get => Docacudiente; set => Docacudiente = value; }
+        [Column("id_ac")]
+        public int id_ac { get => Id_ac; set => Id_ac = value; }
+
     }
 }
