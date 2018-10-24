317d44afadc290d79d0ef7ff394e5281d647e3fd
edwardconw <coyoteluxeredward@outlook.es>
2018-10-24 15:10:16 -0500

    Menc Materia

--- Utilitarios/MEncSeguridad/MEncMateria.cs
+++ Utilitarios/MEncSeguridad/MEncMateria.cs
@@ -10,30 +10,19 @@ namespace Utilitarios.MEncSeguridad
     {
 
 
-        private String Id_materia_nuevo;
+        
         private String Nombre_materia_nuevo;
-        private String Sesion_nuevo;
-        private String Ultima_modificacion_nuevo;
-        private String Id_materia_anterior;
         private String Nombre_materia_old;
-        private String Sesion_old;
-        private String Ultima_modificacion_old;
-        private String id_materia_nuevo;
-        private String Nombre_materia_anterior;
-        private String Sesion_anterior;
-        private String Ultima_modificacion_anterior;
 
-        public string Id_materia_nuevo1 { get => Id_materia_nuevo; set => Id_materia_nuevo = value; }
+        private String Sesion_nuevo;   
+        private String Sesion_old;
+    
+       
+
+        
         public string Nombre_materia_nuevo1 { get => Nombre_materia_nuevo; set => Nombre_materia_nuevo = value; }
         public string Sesion_nuevo1 { get => Sesion_nuevo; set => Sesion_nuevo = value; }
-        public string Ultima_modificacion_nuevo1 { get => Ultima_modificacion_nuevo; set => Ultima_modificacion_nuevo = value; }
-        public string Id_materia_anterior1 { get => Id_materia_anterior; set => Id_materia_anterior = value; }
         public string Nombre_materia_old1 { get => Nombre_materia_old; set => Nombre_materia_old = value; }
         public string Sesion_old1 { get => Sesion_old; set => Sesion_old = value; }
-        public string Ultima_modificacion_old1 { get => Ultima_modificacion_old; set => Ultima_modificacion_old = value; }
-        public string Id_materia_nuevo2 { get => id_materia_nuevo; set => id_materia_nuevo = value; }
-        public string Nombre_materia_anterior1 { get => Nombre_materia_anterior; set => Nombre_materia_anterior = value; }
-        public string Sesion_anterior1 { get => Sesion_anterior; set => Sesion_anterior = value; }
-        public string Ultima_modificacion_anterior1 { get => Ultima_modificacion_anterior; set => Ultima_modificacion_anterior = value; }
     }
 }
