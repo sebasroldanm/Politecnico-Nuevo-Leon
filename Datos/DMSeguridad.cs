a03acd5e4521286edf9db6290c8f16962a582e38
edwardconw <coyoteluxeredward@outlook.es>
2018-10-26 13:54:19 -0500

    DMseguridad Usuario

--- Datos/DMSeguridad.cs
+++ Datos/DMSeguridad.cs
@@ -172,6 +172,28 @@ namespace Datos
         }
 
 
+        public void fiel_auditoria_agrega_usuario(string _accion, string sesion, MEncAcudiente enc)
+        {
+            Auditoria au = new Auditoria();
+            au.fecha = DateTime.Now.ToShortDateString() + " " + DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second;
+            au.accion = _accion;
+            au.schema = "usuario";
+            au.tabla = "usuario";
+            au.pk = "1";
+            au.session = sesion;
+            au.user_bd = "postgres";
+            if (_accion == "INSERT")
+            {
+                au.data = JsonConvert.SerializeObject(enc);
+                using (var db = new Mapeo("public"))
+                {
+                    db.auditoria.Add(au);
+                    db.SaveChanges();
+
+                }
+            }
+
+        }
 
 
     }
