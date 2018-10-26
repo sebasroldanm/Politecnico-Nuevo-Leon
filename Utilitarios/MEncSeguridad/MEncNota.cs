fceb5767508f0ebf9daf29deb2b7b110bb484ad7
edwardconw <coyoteluxeredward@outlook.es>
2018-10-26 12:26:16 -0500

    Menc Nota

--- Utilitarios/MEncSeguridad/MEncNota.cs
+++ Utilitarios/MEncSeguridad/MEncNota.cs
@@ -8,46 +8,39 @@ namespace Utilitarios.MEncSeguridad
 {
     public class MEncNota
     {
-        private String Id_nota_nuevo;
-        private String Id_n_estudiante_nuevo;
-        private String Id_n_materia_nuevo;
+       
+        private Int32 Id_n_estudiante_nuevo;
+        private Int32 Id_n_estudiante_anterior;
+        private Int32 Id_n_materia_nuevo;
+        private Int32 Id_n_materia_old;
         private String Nota1_nuevo;
-        private String Nota2_nuevo;
-        private String Nota3_nuevo;
-        private String Notadef_nuevo;
-        private String Id_nota_anterior;
-        private String Id_n_estudiante_old;
-        private String Id_n_materia_old;
         private String Nota1_old;
+        private String Nota2_nuevo;
         private String Nota2_old;
+        private String Nota3_nuevo;       
         private String Nota3_old;
+        private String Notadef_nuevo;
         private String Notadef_old;
-        private String Id_n_estudiante_anterior;
-        private String Id_n_materia_anterior;
-        private String Nota1_anota1_nuevo;
-        private String Nota2_anterior;
-        private String Nota3_anterior;
-        private String Notadef_anterior;
 
-        public string Id_nota_nuevo1 { get => Id_nota_nuevo; set => Id_nota_nuevo = value; }
-        public string Id_n_estudiante_nuevo1 { get => Id_n_estudiante_nuevo; set => Id_n_estudiante_nuevo = value; }
-        public string Id_n_materia_nuevo1 { get => Id_n_materia_nuevo; set => Id_n_materia_nuevo = value; }
-        public string Nota1_nuevo1 { get => Nota1_nuevo; set => Nota1_nuevo = value; }
-        public string Nota2_nuevo1 { get => Nota2_nuevo; set => Nota2_nuevo = value; }
-        public string Nota3_nuevo1 { get => Nota3_nuevo; set => Nota3_nuevo = value; }
-        public string Notadef_nuevo1 { get => Notadef_nuevo; set => Notadef_nuevo = value; }
-        public string Id_nota_anterior1 { get => Id_nota_anterior; set => Id_nota_anterior = value; }
-        public string Id_n_estudiante_old1 { get => Id_n_estudiante_old; set => Id_n_estudiante_old = value; }
-        public string Id_n_materia_old1 { get => Id_n_materia_old; set => Id_n_materia_old = value; }
-        public string Nota1_old1 { get => Nota1_old; set => Nota1_old = value; }
-        public string Nota2_old1 { get => Nota2_old; set => Nota2_old = value; }
-        public string Nota3_old1 { get => Nota3_old; set => Nota3_old = value; }
-        public string Notadef_old1 { get => Notadef_old; set => Notadef_old = value; }
-        public string Id_n_estudiante_anterior1 { get => Id_n_estudiante_anterior; set => Id_n_estudiante_anterior = value; }
-        public string Id_n_materia_anterior1 { get => Id_n_materia_anterior; set => Id_n_materia_anterior = value; }
-        public string Nota1_anota1_nuevo1 { get => Nota1_anota1_nuevo; set => Nota1_anota1_nuevo = value; }
-        public string Nota2_anterior1 { get => Nota2_anterior; set => Nota2_anterior = value; }
-        public string Nota3_anterior1 { get => Nota3_anterior; set => Nota3_anterior = value; }
-        public string Notadef_anterior1 { get => Notadef_anterior; set => Notadef_anterior = value; }
+        public int id_n_estudiante_nuevo { get => Id_n_estudiante_nuevo; set => Id_n_estudiante_nuevo = value; }
+        public int id_n_estudiante_anterior { get => Id_n_estudiante_anterior; set => Id_n_estudiante_anterior = value; }
+
+        public int id_n_materia_nuevo { get => Id_n_materia_nuevo; set => Id_n_materia_nuevo = value; }
+        public int id_n_materia_old { get => Id_n_materia_old; set => Id_n_materia_old = value; }
+
+        public string nota1_nuevo { get => Nota1_nuevo; set => Nota1_nuevo = value; }
+        public string nota1_old { get => Nota1_old; set => Nota1_old = value; }
+
+        public string nota2_nuevo { get => Nota2_nuevo; set => Nota2_nuevo = value; }
+        public string nota2_old { get => Nota2_old; set => Nota2_old = value; }
+
+        public string nota3_nuevo { get => Nota3_nuevo; set => Nota3_nuevo = value; }
+        public string nota3_old { get => Nota3_old; set => Nota3_old = value; }
+
+        public string notadef_nuevo { get => Notadef_nuevo; set => Notadef_nuevo = value; }
+        public string notadef_old { get => Notadef_old; set => Notadef_old = value; }
+
+      
+        
     }
 }
