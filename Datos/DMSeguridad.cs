4177506e5452e440efbcaae4396f69202984e683
edwardconw <coyoteluxeredward@outlook.es>
2018-10-26 13:05:25 -0500

    MEnc Observador

--- Datos/DMSeguridad.cs
+++ Datos/DMSeguridad.cs
@@ -124,5 +124,29 @@ namespace Datos
         }
 
 
+        public void fiel_auditoria_agrega_orservador(string _accion, string sesion, MEncMateriaFecha enc)
+        {
+            Auditoria au = new Auditoria();
+            au.fecha = DateTime.Now.ToShortDateString() + " " + DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second;
+            au.accion = _accion;
+            au.schema = "registro";
+            au.tabla = "observador";
+            au.pk = "1";
+            au.session = sesion;
+            au.user_bd = "postgres";
+            if (_accion == "INSERT")
+            {
+                au.data = JsonConvert.SerializeObject(enc);
+                using (var db = new Mapeo("public"))
+                {
+                    db.auditoria.Add(au);
+                    db.SaveChanges();
+
+                }
+            }
+
+        }
+
+
     }
 }
---
+++ Utilitarios/MEncSeguridad/MEnsObservador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilitarios.MEncSeguridad
+{
+    public class MEnsObservador
+    {
+        String Observacion_nuevo;
+        String Observacion_old;
+        Int32 Id_estudiante_nuevo;
+        Int32 Id_estudiante_old;
+        String Fecha_observacion_nuevo;
+        String Fecha_observacion_old;
+
+        public string observacion_nuevo { get => Observacion_nuevo; set => Observacion_nuevo = value; }
+        public string observacion_old { get => Observacion_old; set => Observacion_old = value; }
+        public int id_estudiante_nuevo { get => Id_estudiante_nuevo; set => Id_estudiante_nuevo = value; }
+        public int id_estudiante_old { get => Id_estudiante_old; set => Id_estudiante_old = value; }
+        public string fecha_observacion_nuevo { get => Fecha_observacion_nuevo; set => Fecha_observacion_nuevo = value; }
+        public string fecha_observacion_old { get => Fecha_observacion_old; set => Fecha_observacion_old = value; }
+    }
+}
