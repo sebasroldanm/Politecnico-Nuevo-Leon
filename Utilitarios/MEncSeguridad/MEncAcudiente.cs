6be94b55561ece9cbe47d076e8f7779125ffa9b5
edwardconw <coyoteluxeredward@outlook.es>
2018-10-26 21:39:05 -0500

    Private Acudiente

--- Utilitarios/MEncSeguridad/MEncAcudiente.cs
+++ Utilitarios/MEncSeguridad/MEncAcudiente.cs
@@ -8,10 +8,10 @@ namespace Utilitarios.MEncSeguridad
 {
     public class MEncAcudiente
     {
-        Int32 Id_ac_estudiante_nuevo;
-        Int32 Id_ac_estudiante_old;
-        Int32 Id_ac_acudiente_nuevo;       
-        Int32 Id_ac_acudiente_old;
+        private Int32 Id_ac_estudiante_nuevo;
+        private Int32 Id_ac_estudiante_old;
+        private Int32 Id_ac_acudiente_nuevo;
+        private Int32 Id_ac_acudiente_old;
 
         public int id_ac_estudiante_nuevo { get => Id_ac_estudiante_nuevo; set => Id_ac_estudiante_nuevo = value; }
         public int id_ac_estudiante_old { get => Id_ac_estudiante_old; set => Id_ac_estudiante_old = value; }
