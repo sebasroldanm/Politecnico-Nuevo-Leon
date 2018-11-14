81a51ae204942a808840068cdcca1c0db24717b3
edwardconw <coyoteluxeredward@outlook.es>
2018-11-13 22:17:43 -0500

    seguridad Notas

---
+++ Logica/clsSeguridadNotas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+
+
+namespace WebServiceTokens.Logica
+{
+   public class clsSeguridadNotas : System.Web.Services.Protocols.SoapHeader
+    {
+        public string stToken { get; set; }
+        public string AutenticacionToken { get; set; }
+
+        public bool blCredencialesValidas(string stToken)
+        {
+            try
+            {
+                if (stToken == "nota")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool blCredencialesValidas(clsSeguridadNotas SoapHeader)
+        {
+            try
+            {
+                if (SoapHeader == null)
+                {
+                    return false;
+                }
+                if (!string.IsNullOrEmpty(SoapHeader.AutenticacionToken))
+                {
+                    return (HttpRuntime.Cache[SoapHeader.AutenticacionToken] != null);
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
+
+
+
