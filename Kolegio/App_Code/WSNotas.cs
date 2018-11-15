e604905d9667659da63dc7ecbe8499d099750ed4
CamiloRodri <camilorodri28@outlook.com>
2018-11-15 16:24:54 -0500

    WS Notas

---
+++ Kolegio/App_Code/WSNotas.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Utilitarios.MVistasServicios;
+using Datos;
+
+/// <summary>
+/// Descripción breve de WSNotas
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+// [System.Web.Script.Services.ScriptService]
+public class WSNotas : System.Web.Services.WebService
+{
+
+    public WSNotas()
+    {
+
+        //Elimine la marca de comentario de la línea siguiente si utiliza los componentes diseñados 
+        //InitializeComponent(); 
+    }
+
+    public WebServiceTokens.Logica.clsSeguridadNotas SoapHeader;
+
+    [WebMethod]
+    [System.Web.Services.Protocols.SoapHeader("SoapHeader")]
+    public string AutenticacionUsuario()
+    {
+        try
+        {
+            if (SoapHeader == null)
+            {
+                return "-1";
+            }
+            if (!SoapHeader.blCredencialesValidas(SoapHeader.stToken))
+            {
+                return "-1";
+            }
+            string stToken = Guid.NewGuid().ToString();
+            HttpRuntime.Cache.Add(stToken,
+                                    SoapHeader.stToken,
+                                    null,
+                                    System.Web.Caching.Cache.NoAbsoluteExpiration,
+                                    TimeSpan.FromMinutes(30),
+                                    System.Web.Caching.CacheItemPriority.NotRemovable,
+                                    null
+                                    );
+            return stToken;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+
+    [WebMethod]
+    [System.Web.Services.Protocols.SoapHeader("SoapHeader")]
+    public List<NotasEstudiantesVista> NotasConDocumento(int doc)
+    {
+        try
+        {
+            if (SoapHeader == null) throw new Exception("Requiere Validacion");
+
+            if (!SoapHeader.blCredencialesValidas(SoapHeader)) throw new Exception("Requiere Validacion");
+
+            DMUser dmus = new DMUser();
+            return dmus.NotasConDocumento(doc);
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+
+    }
+
+}
