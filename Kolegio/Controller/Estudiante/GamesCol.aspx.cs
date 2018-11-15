f693bf6fb5bcf8f40288273275a8264c3553d230
CamiloRodri <camilorodri28@outlook.com>
2018-11-14 21:27:35 -0500

    Servicio GamesCol Consumido

--- Kolegio/Controller/Estudiante/GamesCol.aspx.cs
+++ Kolegio/Controller/Estudiante/GamesCol.aspx.cs
@@ -9,6 +9,32 @@ public partial class View_Estudiante_GamesCol : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        try
+        {
+            SR_ServicioGamesColNoticias.ServiceToken token = new SR_ServicioGamesColNoticias.ServiceToken();
+            SR_ServicioGamesColNoticias.Facebook_servideSoapClient etiqueta = new SR_ServicioGamesColNoticias.Facebook_servideSoapClient();
 
+            {
+                token.sToken = DateTime.Now.ToString("yyyyMMdd");
+            };
+
+            string sToken = etiqueta.AutenticacionCliente(token);
+
+            if (sToken.Equals("-1"))
+            {
+                Response.Write("<Script Language='JavaScript'>parent.alert('Token invalido');</Script>");
+                throw new Exception("token invalido");
+            }
+            token.AutenticacionToken = sToken;
+            etiqueta.postpc(token);
+ 
+            GridView1.DataSource = etiqueta.noticias(token);
+            GridView1.DataBind();
+        }
+        catch (Exception ex)
+        {
+
+            Response.Write("<Script Language='JavaScript'>parent.alert('" + ex.Message + "');</Script>");
+        }
     }
 }
