f7ec34d7806b40ef268a45df3446cd526958bed3
CamiloRodri <camilorodri28@outlook.com>
2018-11-15 15:33:24 -0500

    NotaEstudianteEntity

--- Utilitarios/MVistasServicios/NotasEstudiantesVista.cs
+++ Utilitarios/MVistasServicios/NotasEstudiantesVista.cs
@@ -11,18 +11,19 @@ namespace Utilitarios.MVistasServicios
         private String nombre;
         private String apellido;
         private String curso;
-        private String nota1;
-        private String nota2;
-        private String nota3;
-        private String notaDef;
+        private String materia;
+        private Double nota1;
+        private Double nota2;
+        private Double nota3;
+        private Double notaDef;
 
         public string Nombre { get => nombre; set => nombre = value; }
         public string Apellido { get => apellido; set => apellido = value; }
         public string Curso { get => curso; set => curso = value; }
-        public string Nota1 { get => nota1; set => nota1 = value; }
-        public string Nota2 { get => nota2; set => nota2 = value; }
-        public string Nota3 { get => nota3; set => nota3 = value; }
-        public string NotaDef { get => notaDef; set => notaDef = value; }
-        
+        public string Materia { get => materia; set => materia = value; }
+        public double Nota1 { get => nota1; set => nota1 = value; }
+        public double Nota2 { get => nota2; set => nota2 = value; }
+        public double Nota3 { get => nota3; set => nota3 = value; }
+        public double NotaDef { get => notaDef; set => notaDef = value; }
     }
 }
