3bcd9811a6f766af8a43c3972fa93f4a91f441a7
edwardconw <coyoteluxeredward@outlook.es>
2018-11-16 11:10:56 -0500

    Clase Plato y Uniempleo vista

--- Kolegio/View/Profesor/Uniempleo.aspx
+++ Kolegio/View/Profesor/Uniempleo.aspx
@@ -1,4 +1,5 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/View/Profesor/MasterProfesor.master" AutoEventWireup="true" CodeFile="~/Controller/Profesor/Uniempleo.aspx.cs" Inherits="View_Profesor_Uniempleo" %>
+﻿
+<%@ Page Title="" Language="C#" MasterPageFile="~/View/Profesor/MasterProfesor.master" AutoEventWireup="true" CodeFile="~/Controller/Profesor/Uniempleo.aspx.cs" Inherits="View_Profesor_Uniempleo" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
---
+++ Utilitarios/ClasesServicios/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Utilitarios.ClasesServicios
+{
+   public class Usuario
+    {
+
+        private String nombre;
+        private String apellido;
+        private String email;
+        private String User_Name;
+        private String clave;
+        private String rclave;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Email { get => email; set => email = value; }
+        public string User_Name1 { get => User_Name; set => User_Name = value; }
+        public string Clave { get => clave; set => clave = value; }
+        public string Rclave { get => rclave; set => rclave = value; }
+    }
+}
